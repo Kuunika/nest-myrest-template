79a366b5d98aca538ecf5c3e90360171f760d3c6
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-09-27 07:29:51 +0200

    refactor(app): register typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,29 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
 import helmet from 'helmet';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { API_MONGODB_URI } from './common/constants';
 import { LogModule } from './log/log.module';
 import { HealthController } from './health/health.controller';
+import { API_MYSQL_URI } from './common/constants';
 
 @Module({
   imports: [
-    MongooseModule.forRootAsync({
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (service: ConfigService) => ({
-        uri: service.get<string>(API_MONGODB_URI),
+      useFactory: (service: ConfigService): TypeOrmModuleOptions => ({
+        type: 'mysql',
+        url: service.get<string>(API_MYSQL_URI),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        logging: ['error', 'warn'],
+        logger: 'file',
       }),
       inject: [ConfigService],
     }),
-    ConfigModule.forRoot({ isGlobal: true }),
     LogModule,
     TerminusModule,
     HttpModule,
